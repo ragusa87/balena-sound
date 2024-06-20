6b7e5a5d05bf17fc4502adcdd49964739a76b36b
Laurent Constantin <laurent.constantin@liip.ch>
2024-06-20 20:40:26 +0200

    Add ir support and MPD control via dbus
    Add MPD-mpris to control mpd with dbus

---
+++ plugins/mpd-mpris/start.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+set -ex
+
+# Exit if requested
+if [ -n "$SOUND_DISABLE_MPD_MPRIS" ]; then
+  echo "MPD MPRIS is disabled, exiting..."
+  exit 0
+fi
+
+# Parse config
+DEFAULT_HOST="localhost"
+MPD_HOST=${SOUND_MPD_HOST:-"$DEFAULT_HOST"}
+
+
+RUN_COMMAND=$(command -v mpd-mpris)
+# if command starts with an option, prepend mpd-mpris
+if [ -n "$1" ] && [ "$(echo "$1" | cut -c1)" = '-' ]; then
+  set -- ${RUN_COMMAND} "$@"
+fi
+
+# Add default arguments
+if [ "$1" = "${RUN_COMMAND}" ]; then
+  shift
+  set -- ${RUN_COMMAND} -no-instance -host "${MPD_HOST}" "$@"
+fi
+
+exec "$@"
